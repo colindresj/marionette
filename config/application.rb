8502d503a64b5514a35fa6889e05eef82e4ddca3
JC <jorge.e.colindres@gmail.com>
2013-12-27 16:24:20 -0500

    added sticky footer

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,6 @@ module Marionette
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    HandlebarsAssets::Config.template_namespace = 'JST'
+    # HandlebarsAssets::Config.template_namespace = 'JST'
   end
 end
