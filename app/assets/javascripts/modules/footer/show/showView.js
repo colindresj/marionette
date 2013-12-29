b96423e82cbb6af7af010c55e0320d57c1cb5b31
JC <jorge.e.colindres@gmail.com>
2013-12-29 00:03:26 -0500

    added a model event listener for the header composite view. listens to changes on App.currentUser model and fires off the render for the view on change. changed appendHtml function on this view as well to prepend on itemViewContainer instead of append

--- app/assets/javascripts/modules/footer/show/showView.js
+++ app/assets/javascripts/modules/footer/show/showView.js
@@ -1,10 +1,7 @@
 App.module('FooterModule.Show', function(Show, App, Backbone, Marionette, $, _){
 
   Show.FooterView = App.Views.MainItemView.extend({
-    template: 'footer/show/templates/show',
-    modelEvents: {
-      'change': 'render'
-    }
+    template: 'footer/show/templates/show'
   });
 
 });
--- app/assets/javascripts/modules/header/list/listView.js
+++ app/assets/javascripts/modules/header/list/listView.js
@@ -2,12 +2,19 @@ App.module('HeaderModule.List', function(List, App, Backbone, Marionette, $, _){
 
   List.HeaderView = App.Views.MainItemView.extend({
     tagName: 'li',
-    template: 'header/list/templates/_each'
+    template: 'header/list/templates/_links'
   });
 
   List.HeadersView = App.Views.MainCompositeView.extend({
     template: 'header/list/templates/list',
     itemView: List.HeaderView,
-    itemViewContainer: 'ul.navbar-right'
+    itemViewContainer: 'ul.navbar-right',
+    appendHtml: function(collectionView, itemView){
+      var $el = collectionView.$el.find(collectionView.itemViewContainer);
+      $el.prepend(itemView.el);
+    },
+    initialize: function(){
+      this.listenTo(App.currentUser, 'change', this.render);
+    }
   });
 });
